65bca12a73c6bc8fb1d61ca8127ea226e612d15e
DanielMRX777 <140866516+katia-silva09@users.noreply.github.com>
2023-11-01 17:10:05 -0600

    editLogin
    editandi login para guardar en LocalStore

--- js/login.js
+++ js/login.js
@@ -1,49 +1,78 @@
-let users =[]; //lista de usuarios
+let users = []; // Lista de usuarios
 
-const data = { //objetusers
-    username: '',
-    password:''
-        };
-
-
-let editando =false;
-
-const datausers = document.querySelector('#datausers'), //
-    submit = document.querySelector('#submit'),
-    usernameInput = document.querySelector("#username"),
-    passwordInput = document.querySelector("#password"),
-    visible = document.querySelector("#visible"),
-    reset = document.querySelector("#reset");
+const data = { // Objeto usuarios
+  username: "",
+  password: ""
+};
 
 
+const datausers = document.querySelector("#datausers"), // Formulario
+  usernameInput = document.querySelector("#username"),
+  passwordInput = document.querySelector("#password"),
+  visible = document.querySelector("#visible"),
+  reset = document.querySelector("#reset");
 
-       
-    document.addEventListener("change", (e) =>{
-        if (e.target ===visible){
-            if(visible.checked === false)passwordInput.type ="password";
-            else passwordInput.type = "text";
-        }
-    })
+submit.addEventListener("click", (e) => {
+  e.preventDefault(); // Prevenir el envío del formulario
 
-
-    document.addEventListener("click",(e) =>{
-        if (e.target ===reset ){
-            if(passwordInput.value !== "" && usernameInput.value !== "" ){
-                e.preventDefault();
-                alert("registrado, ahora puedes iniciar sesion")
-                window.location.href= "sigin.html"
-
-            }else {
-                alert("Es necesario llenar todos los campos")
-            }
-        }
+  const inputUsername = usernameInput.value;
+  const inputPassword = passwordInput.value;
 
 
- 
-function adduser(){
-        users.push({...data});
-    
-        datausers.reset();
+  // Obtener los datos del usuario del almacenamiento local
+const user = JSON.parse(localStorage.getItem('user'));
+
+
+  if (user.username === inputUsername) {
+    // Usuario encontrado, verificar contraseña
+    if (user.password === inputPassword) {
+      // Contraseña correcta, iniciar sesión
+      alert("Inicio de sesión exitoso");
+      window.location.href = "index.html"; // Redirigir a la página de inicio de sesión
+    } else {
+      alert("Contraseña incorrecta");
+    }
+  } else {
+    alert("Usuario no encontrado");
+  }
+});
+
+reset.addEventListener("click", (e) => {
+  if (passwordInput.value !== "" && usernameInput.value !== "") {
+    e.preventDefault();
+    data.username = usernameInput.value;
+    data.password = passwordInput.value;
+
+    // Guardar los datos del usuario en el almacenamiento local
+    localStorage.setItem('user', JSON.stringify(data));
+
+    alert("Registrado, ahora puedes iniciar sesión.");
+    users.push({ ...data }); // Agrega el usuario a la lista
+    window.location.href = "index.html";
+  } else {
+    alert("Es necesario llenar todos los campos.");
+  }
+});
+
+visible.addEventListener("change", (e) => {
+  if (e.target === visible) {
+    if (visible.checked === false) {
+      passwordInput.type = "password";
+    } else {
+      passwordInput.type = "text";
+    }
+  }
+});
+
+function adduser() {
+  // Guardar los datos del usuario en el almacenamiento local
+  localStorage.setItem('user', JSON.stringify(data));
+
+  // Redirigir al usuario al index.html
+  window.location.href = 'index.html';
 }
+
+function limpiarObjet() {
+  data.username = '';
+  data.password = '';
 }
-)
